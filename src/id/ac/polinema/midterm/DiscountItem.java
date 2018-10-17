0ca9a6edac44ee01a4bf6c9aa1555d8fa226a3e5
User <User@DEKSTOP-ArkaLeonant>
2018-10-17 20:06:37 +0700

    progres

---
+++ src/id/ac/polinema/midterm/DiscountItem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polinema.midterm;
+
+/**
+ *
+ * @author User
+ */
+class DiscountItem {
+    private float discount;
+
+    public DiscountItem(String name, float price, int amount, float discount) {
+        this.discount = discount;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+    
+    
+}
---
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polinema.midterm;
+
+/**
+ *
+ * @author User
+ */
+class PromoItem {
+    private float promo;
+
+    public PromoItem(String name, float price, int amount, float promo) {
+        this.promo = promo;
+    }
+
+    public float getPromo() {
+        return promo;
+    }
+
+    public void setPromo(float promo) {
+        this.promo = promo;
+    }
+    
+    
+}
