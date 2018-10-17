8478fe5d380cd51194d5309537c9df66df49b19e
Angga Maulana A <Angga Maulana A@PaceDjawa>
2018-10-17 20:48:48 +0700

    Menambahkan Atribut di class PromoItem

---
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polinema.midterm;
+
+/**
+ *
+ * @author Angga Maulana A
+ */
+class PromoItem {
+    private float promo;
+
+    public PromoItem(float promo) {
+        this.promo = promo;
+    }
+
+    PromoItem(String kopi, int i, int i0, int i1) {
+    }
+
+    public float getPromo() {
+        return promo;
+    }
+
+    public void setPromo(float promo) {
+        this.promo = promo;
+    }
+}
