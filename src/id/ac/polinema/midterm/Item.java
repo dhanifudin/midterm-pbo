f68b0d2f4ff78def6d3f979d7da7ca3ba400b607
Toshiba <Toshiba@DESKTOP-CMU4KA8>
2018-10-17 20:46:53 +0700

    mengisi method

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -11,5 +11,24 @@ package id.ac.polinema.midterm;
  * @author dhanifudin
  */
 public class Item {
+    
+    protected String nama;
+    protected float price;
+    protected int amount;
 
+    public Item(String nama, float price, int amount) {
+        this.nama = nama;
+        this.price = price;
+        this.amount = amount;
+    }
+    
+    public float getTotalPrice(){
+        return price = price;
+    }
+    public String toString(){
+        return nama = nama;
+    }
+    public int toInt(){
+        return int = int;
+    }
 }
