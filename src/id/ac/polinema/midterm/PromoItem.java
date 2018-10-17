b571819f075acf399d8c28cf9c89737dbc43d769
fawwaz <>
2018-10-17 21:19:38 +0700

    penambahan dan pengeditan class

---
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polinema.midterm;
+
+/**
+ *
+ * @author iloops
+ */
+public class PromoItem {
+    private float promo;
+    
+}
