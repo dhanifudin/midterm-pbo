5c51fbe9fb066d243b885918469502a740ca7595
asus <asus@DESKTOP-Q3EQCNE.kuikdelivery.com>
2018-10-17 21:50:00 +0700

    memasukkan atribut dan method pada class Item

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -26,7 +26,7 @@ public class Item {
     }
     
     public String toString(){
-        return name+" \t\t"+price+" \t\t"+amount;
+        return "|"+(name)+" \t\t|"+(price)+"\t|"+(amount)+"\t\t|"+(price*amount)+"\t|";
     }
     
 }
