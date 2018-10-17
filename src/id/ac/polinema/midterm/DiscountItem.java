55064950a4aae6a51ba78a4b02d196704030de5c
akmalkhosy@gmail.com <akmal130198>
2018-10-17 23:11:22 +0700

    saya sudah memberikan komentar di class DiscountItem pak

--- src/id/ac/polinema/midterm/DiscountItem.java
+++ src/id/ac/polinema/midterm/DiscountItem.java
@@ -12,22 +12,25 @@ package id.ac.polinema.midterm;
 public class DiscountItem extends Item{
     private float discount;
 
+    //Kontruktor ini saya buat sama dengan yang ada di class Main dan menghilangkan error yang ada
     public DiscountItem(String name, float price, int amount, float discount) {
         super(name, price, amount);
         this.discount = discount*100;
     }
     
-    public float getDiscount(){
-        
+    //method ini saya bikin untuk menghitung Operasi diskon 
+    public float getDiscount(){        
         float disc = (this.discount/100) * (this.price*this.amount);
         return disc;
     }
     
-    
+    //method ini saya bikin untuk menghitung total harga yang telah di masukan atribut diskon
     @Override
     public float getTotalPrice(){
         return this.amount * this.price -getDiscount();
     } 
+    
+   //method bikin untuk menampiklan informasi yang telah di berikan diskon.
     @Override
     public String toString(){
         return "|"+(this.name)+"\t\t|"+(this.price)+"\t|"+(this.amount)+"\t\t"+getTotalPrice()+"\t|"+"\n|-Disc"+"\t\t|"+this.discount+"%\t\t\t";
