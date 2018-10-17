2744b6c4b20c8d99c942425e683a606cec00a19f
Alfaza <Alfaza@DESKTOP-34DV726>
2018-10-17 23:13:26 +0700

    Saya download zip bapak bukan clone jadi saya coding sudah ketiga kalinya ini. Pertama masalah saya ke tekan pull bukan push. Ini murni kerjaan saya

---
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -0,0 +1,11 @@
+package id.ac.polinema.midterm;
+
+/**
+ *
+ * @author Alfaza
+ */
+public class PromoItem extends Item{
+    private float promo;
+    
+    //PromoItem merupakan subclass dari class Item
+}
