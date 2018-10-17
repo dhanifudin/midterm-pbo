2744433349a95cdbd44027f701e3942f1ae8406b
hp <hp@DESKTOP-9C5A2KE>
2018-10-17 22:08:35 +0700

    membuat constrator dan method

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -11,5 +11,24 @@ package id.ac.polinema.midterm;
  * @author dhanifudin
  */
 public class Item {
+    protected String nama;
+    protected float price;
+    protected int amount;
 
+    public Item() {
+    }
+
+    public Item(String nama, float price, int amount) {
+        this.nama = nama;
+        this.price = price;
+        this.amount = amount;
+    }
+    
+   public float getTotalPrice(){
+       return price * amount;
+   }
+   
+   public String toString(){
+       return nama;
+   }
 }
