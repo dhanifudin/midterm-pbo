891dcc3bb66573894a3f318ea7ecdf07a6cc3ccc
MSI Notebook <MSI Notebook@DESKTOP-2D6BNE3>
2018-10-17 22:40:12 +0700

    Perbaikan 4 pada item

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -23,34 +23,6 @@ public class Item {
         this.amount = amount ;
     }
     
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-    
-    public float getTotalPrice() {
-        return (amount*price) ;
-    }
-    
     @Override
     public String toString() {
         return "|" + (name) + "\t\t|" + (price) + "\t|" + (amount) + "\t\t|" + (price*amount)+ "/|" ;
