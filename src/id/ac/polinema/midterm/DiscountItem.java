0351d790436a902eea149e704ef5e5aa94d54c0a
Rivo <Rivo@DESKTOP-91Q61VH>
2018-10-17 23:29:38 +0700

    mengerjakan isian methodnya dalam discountItem

--- src/id/ac/polinema/midterm/DiscountItem.java
+++ src/id/ac/polinema/midterm/DiscountItem.java
@@ -9,6 +9,26 @@ package id.ac.polinema.midterm;
  *
  * @author Rivo
  */
-public class DiscountItem {
+public class DiscountItem extends Item{
+    private float discount;
     
+    public DiscountItem(String name, float price, int amount, float discount){
+        super(name, price, amount);
+        this.discount = discount;
+    }
+    //membuat constructornya menggunakan super
+    
+    public float getDiscount(){
+        return(amount*price)*discount;
+    }
+    //menghitung diskonnya
+    
+    public float getTotalPrice(){
+        return amount*price-getDiscount();
+    }
+    //menghitung jumlahnya
+    
+    public String toString(){
+        
+    }
 }
