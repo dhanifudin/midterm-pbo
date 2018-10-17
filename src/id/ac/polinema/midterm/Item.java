7d74b3bbaffff8a7977acf9fa9c83acfe16efd31
ASUS <ASUS@192.168.56.1>
2018-10-17 21:37:56 +0700

    Memasukkan atribut, konstruktor dan method pada class Item

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -11,5 +11,21 @@ package id.ac.polinema.midterm;
  * @author dhanifudin
  */
 public class Item {
+    protected String name;
+    protected float price;
+    protected int amount;
 
+    public Item(String name, float price, int amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+    
+    public float getTotalPrice(){
+        return (int)(amount*price);
+    }
+    
+    public String toString(){
+        return name+" \t\t"+price+" \t\t"+amount;
+    }
 }
