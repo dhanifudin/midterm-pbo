1e0ca0dede5a0c8a546543c751350b19debd4706
admj <admj@admj-K84L>
2018-10-17 23:27:07 +0700

    menambah atribut dan fungsi di class discountitem

--- src/id/ac/polinema/midterm/DiscountItem.java
+++ src/id/ac/polinema/midterm/DiscountItem.java
@@ -9,6 +9,26 @@ package id.ac.polinema.midterm;
  *
  * @author admj
  */
-public class DiscountItem {
-    
+public class DiscountItem extends Item{
+    private float discount;
+
+    public DiscountItem(String nama, float price, int amount, float discount) {
+        super(nama, price, amount);
+        this.discount=discount;
+    }
+
+    public float getDiscount() {
+        return price*discount;
+    }
+   public float Discount(){
+       return price-getDiscount();
+   }
+   public float total(){
+       return amount*Discount();
+   }
+
+    @Override
+    public String toString() {
+        return "Disc "+5.0+"%";
+    }
 }
