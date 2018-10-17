55fbffd6ca72cca764e82c5ce41bd44744f5b1f9
akmalkhosy@gmail.com <akmal130198>
2018-10-17 23:01:14 +0700

    saya sudah memberikan komentar di class PeomoItem pak

--- src/id/ac/polinema/midterm/PromoItem.java
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -12,27 +12,19 @@ package id.ac.polinema.midterm;
 public class PromoItem extends Item{
     private float promo;
 
+    //Kontruktor ini saya buat sama dengan yang ada di class Main agar tidak error
     public PromoItem(String name, float price, int amount, float promo) {
         super(name, price, amount);
         this.promo = promo;
     }
 
-    
-
-
-    public void setPromo(float promo) {
-        this.promo = promo;
-    }
-
-    public float getPromo() {
-        return promo;
-    }
-    
+    //method ini berguna untuk menghitung total harga dengan mamasukan atribut promo
     @Override
     public float getTotalPrice(){
         return (this.amount*this.price)-(this.promo*this.amount);
     }
     
+    //method ini beguna untuk memberikan informasi tentang promo yang telah di buat.
     @Override
     public String toString(){
         return "|"+(this.name)+"\t\t|"+(this.price)+"\t|"+(this.amount)+"\t\t"+getTotalPrice()+"\t|"+"\n|-Promo"+"\t\t|"+this.promo+"\t\t\t";
