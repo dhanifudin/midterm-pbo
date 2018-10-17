a84547df0dd4dbb3703da1820f9e7e5ca5a9c577
KINTUL <KINTUL@DESKTOP-6GMU9JK>
2018-10-17 21:52:19 +0700

    Ubah discount karena 5.0 % itu 0.5 bukan 0.05

--- src/id/ac/polinema/midterm/MidtermTest.java
+++ src/id/ac/polinema/midterm/MidtermTest.java
@@ -14,7 +14,7 @@ public class MidtermTest {
 
 	public static void main(String[] args) {
 		Item item = new Item("Gula", 10000, 2);
-		DiscountItem discountItem = new DiscountItem("Susu", 10000, 2, 0.05f);
+		DiscountItem discountItem = new DiscountItem("Susu", 10000, 2, 0.5f);
 		PromoItem promoItem = new PromoItem("Kopi", 10000, 2, 2000);
 
 		Transaction transaction = new Transaction(item, discountItem, promoItem);
