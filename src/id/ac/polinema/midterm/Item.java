8fc4b1d558ed59400f755b060270858916db7a6a
Yulica <Yulica@192.168.56.1>
2018-10-17 20:30:59 +0700

    tambah class baru

---
+++ src/id/ac/polinema/midterm/Item.java
@@ -0,0 +1,15 @@
+package id.ac.polinema.midterm;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dhanifudin
+ */
+public class Item {
+
+}
