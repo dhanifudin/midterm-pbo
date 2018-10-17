9177e358236824e16bde310365ad503f24b4ac38
MSI Notebook <MSI Notebook@DESKTOP-2D6BNE3>
2018-10-17 21:09:13 +0700

    Membuat Class Item

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -12,4 +12,54 @@ package id.ac.polinema.midterm;
  */
 public class Item {
 
+    protected String name ;
+    protected float price ;
+    protected int amount ;
+    
+    
+    public Item(String name , float price , int amount) {
+        this.name = name ;
+        this.price = price ;
+        this.amount = amount ;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
+    public float getTotalPrice() {
+        return (amount*price) ;
+    }
+    
+    @Override
+    public String toString() {
+        return name ;
+    }
+    
+    public void print() {
+    
+    
+    
+        
+}
 }
