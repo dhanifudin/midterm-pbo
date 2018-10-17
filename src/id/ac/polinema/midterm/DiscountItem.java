b262cd8fa1c11f49f2d9064bf76e757a0df918a6
jajal <jajal@starZ>
2018-10-17 22:12:31 +0700

    mengembalikan class discountitem yg terhapus

---
+++ src/id/ac/polinema/midterm/DiscountItem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polinema.midterm;
+
+/**
+ *
+ * @author jajal
+ */
+public class DiscountItem extends Item{
+    private float discount;
+
+    public DiscountItem(float discount) {
+        this.discount = discount;
+    }
+
+    public DiscountItem( String name, float price, int amount,float discount) {
+        super(name, price, amount);
+        this.discount = discount;
+    }
+    public float setDiskon(){
+        return (amount * price)-(amount * price) * discount; 
+    }
+    
+    
+}
