29803a3d8ba558729a74f8700af94ec945f19c01
Toshiba <Toshiba@DESKTOP-CMU4KA8>
2018-10-17 21:38:44 +0700

    membuat class baru PromoItem dan konstrakturnya

---
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polinema.midterm;
+    public class PromoItem extends Item{
+        
+        private float promo;
+        
+        public PromoItem(){
+        }
+        
+        public PromoItem(String name, float price, int amount, float promo){
+            super(name, price, amount);
+            this.promo = promo;
+        }
+        }
