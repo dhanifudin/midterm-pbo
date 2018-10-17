518442b7a6d987bdea8a4208aa28758c3399f107
hp <hp@DESKTOP-9C5A2KE>
2018-10-17 22:22:25 +0700

    membuat class Discount Item dan membuat constraktor

---
+++ src/id/ac/polinema/midterm/DiscountItem.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polinema.midterm;
+
+/**
+ *
+ * @author hp
+ */
+class DiscountItem extends Item {
+    private float discount;
+    
+    public DiscountItem(){
+        
+    }
+
+    public DiscountItem(String nama, float price, int amount, float promo) {
+        super(nama, price, amount);
+        this.discount = discount;
+    }
+
+    
+    
+}
