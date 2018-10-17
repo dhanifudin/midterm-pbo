38194872a4eb581579a4e30f3538fcafbb94863a
hp <hp@Arifannyrs>
2018-10-17 20:12:59 +0700

    commit1

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -11,5 +11,39 @@ package id.ac.polinema.midterm;
  * @author dhanifudin
  */
 public class Item {
+    private String name;
+    private float price;
+    private int amount;
 
+    public Item(String name, float price, int amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
+    
 }
---
+++ src/id/ac/polinema/midterm/Promoitem.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polinema.midterm;
+
+/**
+ *
+ * @author Arifanny Ramadhan Sukma / 1741720217 / TI=2A
+ */
+public class Promoitem {
+    private float promo;
+
+    public Promoitem(float promo) {
+        this.promo = promo;
+    }
+
+    public float getPromo() {
+        return promo;
+    }
+
+    public void setPromo(float promo) {
+        this.promo = promo;
+    }
+    
+}
