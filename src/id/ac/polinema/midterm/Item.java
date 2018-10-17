597d2978ab1f10d1bfe75ca949fd7466722efa45
KINTUL <KINTUL@DESKTOP-6GMU9JK>
2018-10-17 21:42:24 +0700

    Menghapus setter getter tak terpakai

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -25,30 +25,6 @@ public class Item {
         this.amount = amount;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-    
     public float getTotalPrice(){
         return price * amount;
     }
