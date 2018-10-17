e74437626075712628e2438f7c4bb1e3cb55cd75
Angga Maulana A <Angga Maulana A@PaceDjawa>
2018-10-17 20:48:25 +0700

    Menambahkan Atribut di class DiscountItem

---
+++ src/id/ac/polinema/midterm/DiscountItem.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polinema.midterm;
+
+/**
+ *
+ * @author Angga Maulana A
+ */
+class DiscountItem {
+    private float discount;
+
+    public DiscountItem(float discount) {
+        this.discount = discount;
+    }
+
+    DiscountItem(String susu, int i, int i0, float f) {
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+    
+    
+    
+}
