21b9797f2818793ae4678fa1770bc448ce553787
Angga Maulana A <Angga Maulana A@PaceDjawa>
2018-10-17 20:52:22 +0700

    Menambahkan Atribut di class PromoItem

--- src/id/ac/polinema/midterm/DiscountItem.java
+++ src/id/ac/polinema/midterm/DiscountItem.java
@@ -9,14 +9,12 @@ package id.ac.polinema.midterm;
  *
  * @author Angga Maulana A
  */
-class DiscountItem {
+class DiscountItem extends Item {
     private float discount;
 
-    public DiscountItem(float discount) {
+    public DiscountItem(float discount, String Nama, float Price, int amount) {
+        super(Nama, Price, amount);
         this.discount = discount;
-    }
-
-    DiscountItem(String susu, int i, int i0, float f) {
     }
 
     public float getDiscount() {
@@ -26,7 +24,31 @@ class DiscountItem {
     public void setDiscount(float discount) {
         this.discount = discount;
     }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+
+    public float getPrice() {
+        return Price;
+    }
+
+    public void setPrice(float Price) {
+        this.Price = Price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
     
+    }
     
-    
-}
+
