1bec76e5bc0b6abbbb4e0a340099fe80056a9d4b
ASUS <ASUS@DESKTOP-23494D9>
2018-10-17 23:19:32 +0700

    membuat class discount, yang mengulang gara gara gagal PUSH pak .. :(

---
+++ src/id/ac/polinema/midterm/DiscountItem.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polinema.midterm;
+
+/**
+ *
+ * @author Tika Yln
+ */
+public class DiscountItem extends Item {
+    
+    private float discount;
+    
+    public DiscountItem(String name, float price, int amount, float discount){
+        super(name, price, amount);
+        this.discount = discount;
+    }
+    
+    public float getDiscount(){
+        return (amount*price)*discount;
+    }
+    
+    public float getTotalPrice(){
+        return amount*price-getDiscount();
+    }
+    
+    public String toString(){
+        return "|"+(name)+"\t\t|"+(price)+"\t|"+(amount)+"\t\t"+getTotalPrice()+"\t|\n|-Discount\t|"+discount*100+"%\t\t|\t\t|\t\t|";
+    }
+    
+}
