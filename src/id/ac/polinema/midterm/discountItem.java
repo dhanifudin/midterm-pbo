935f6968921e3c635ff35307eec31791cc1b6453
Haliza Hayyu Firdausy <halizahayyu@gmail.com>
2018-10-18 00:30:30 +0700

    commit

--- src/id/ac/polinema/midterm/discountItem.java
+++ src/id/ac/polinema/midterm/discountItem.java
@@ -12,21 +12,25 @@ package id.ac.polinema.midterm;
 public class discountItem extends Item {
     private float discount;
     
+    //Konstruktor class discountItem
     public discountItem(String name, float price, int amount, float discount) {
         super(name, price, amount);
         this.discount = discount * 100;
     }
     
+    //Method untuk mereturn jumlah diskon
     public float getDiscount() {
         float diskon = (this.discount / 100) * super.getTotal();
         return diskon;
     }
     
+    //Method override dari class Item
     @Override
     public float getTotal() {
         return super.getTotal() - getDiscount();
     }
     
+    //Method untuk menampilkan isi dari discoutnItem
     public String toString() {
         String tostring = "";
         tostring += "| "+this.name+" \t\t| "+this.price+" \t| "+this.amount+" \t\t| "+ this.getTotal() +"\t|\n";
