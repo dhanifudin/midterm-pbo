c8ed0da38c1ed10d2cb53fe2bdb87047e7b716e5
Haliza Hayyu Firdausy <halizahayyu@gmail.com>
2018-10-18 00:23:33 +0700

    commit

---
+++ src/id/ac/polinema/midterm/discountItem.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polinema.midterm;
+
+/**
+ *
+ * @author haliz
+ */
+public class discountItem extends Item {
+    private float discount;
+    
+    public discountItem(String name, float price, int amount, float discount) {
+        super(name, price, amount);
+        this.discount = discount * 100;
+    }
+    
+    public float getDiscount() {
+        float diskon = (this.discount / 100) * super.getTotal();
+        return diskon;
+    }
+    
+    @Override
+    public float getTotal() {
+        return super.getTotal() - getDiscount();
+    }
+    
+    public String toString() {
+        String tostring = "";
+        tostring += "| "+this.name+" \t\t| "+this.price+" \t| "+this.amount+" \t\t| "+ this.getTotal() +"\t|\n";
+        tostring += "| -Disc\t\t| "+this.discount+" | \t\t | \t | \t |";
+        return tostring;
+    }
+}
