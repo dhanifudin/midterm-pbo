bca9430315a0300739bb933efd1c1405b7def662
ASUS <ASUS@DESKTOP-23494D9>
2018-10-17 23:32:52 +0700

    membuat Class PromoItem

---
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -0,0 +1,30 @@
+package id.ac.polinema.midterm;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tika Yln
+ */
+public class PromoItem extends Item {
+    
+    private float promo;
+    
+    public PromoItem(String name, float price, int amount, float promo){
+        super(name, price, amount);
+        this.promo = promo;
+    }
+    
+    public float getTotalPrice(){
+        return (amount*price)-(promo*amount);
+    }
+    
+    public String toString(){
+        return "|"+(name)+"\t\t|"+(price)+"\t|"+(amount)+"\t\t|"+getTotalPrice()+"\t|\n|-Promo\t\t/|"+promo+"%\t|\t\t|\t\t|";
+    }
+    
+}
