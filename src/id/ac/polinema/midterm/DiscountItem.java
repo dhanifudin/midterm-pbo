68b924a63e016862a8470f5984e36d312c9ec044
User <User@DEKSTOP-ArkaLeonant>
2018-10-17 20:50:17 +0700

    hampir final

--- src/id/ac/polinema/midterm/DiscountItem.java
+++ src/id/ac/polinema/midterm/DiscountItem.java
@@ -10,7 +10,10 @@ package id.ac.polinema.midterm;
  * @author User
  */
 class DiscountItem {
+    private String name;
     private float discount;
+    private float price;
+    private int amount;
 
     public DiscountItem(String name, float price, int amount, float discount) {
         this.discount = discount;
@@ -23,6 +26,35 @@ class DiscountItem {
     public void setDiscount(float discount) {
         this.discount = discount;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
     
     
+    
+    public float DiscountItem(){
+        discount = price - (price* amount * discount);
+        return discount;
+    }
 }
--- src/id/ac/polinema/midterm/PromoItem.java
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -10,7 +10,10 @@ package id.ac.polinema.midterm;
  * @author User
  */
 class PromoItem {
+    private String name;
     private float promo;
+    private float price;
+    private int amount;
 
     public PromoItem(String name, float price, int amount, float promo) {
         this.promo = promo;
@@ -23,6 +26,35 @@ class PromoItem {
     public void setPromo(float promo) {
         this.promo = promo;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
     
     
+    
+    public float PromoItem(){
+        promo = (price * amount) - promo;
+        return promo;
+    }
 }
