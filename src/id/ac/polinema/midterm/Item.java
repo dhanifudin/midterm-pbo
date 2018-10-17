389cf5f86b594e75878232de09402ade3c071f13
izmirtais <izmirtais28@gmail.com>
2018-10-17 22:31:54 +0700

    step

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -22,6 +22,31 @@ public class Item {
         this.price = price;
         this.amount = amount;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
     //menambahkan method
     public float getTotalPrice(){
         float totalprice = price * amount;
