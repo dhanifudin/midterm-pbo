7c2825a4371809c5743344e460682da27c1b57cc
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2018-10-18 09:01:11 +0700

    menambahkan komen deskriptif di class Promo item

--- src/id/ac/polinema/midterm/PromoItem.java
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -10,20 +10,23 @@ package id.ac.polinema.midterm;
  * @author admj
  */
 public class PromoItem extends Item{
+    //menambahkan atribut promo bertipe float
     private float promo;
-
+    //menambahkan konstruktor
     public PromoItem(String nama, float price, int amount, float promo) {
         super(nama, price, amount);
         this.promo= promo;
     }
-
+    //fungsi getPromo berguna untuk menghitung promo dengan mengurangi price dengan promo
     public float getPromo() {
         return price-promo;
     }
+    //fungsi total berguna untuk mendapatkan total harga yang sudah mendapat promo
     public float total(){
+        //mengalikan amount dengan getPromo
         return amount*getPromo();
     }
-
+    //mengubah nilai dari class parrent
     @Override
     public String toString() {
         return "-Promo "+promo;
