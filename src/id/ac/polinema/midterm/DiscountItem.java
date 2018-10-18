a2e946e5a695f245e4c585855cbd7321677dfc67
WINDOWS 10 <WINDOWS 10@Windows10>
2018-10-18 08:04:36 +0700

    menambahkan komentar pada setiap method dari class DiscountItem

--- src/id/ac/polinema/midterm/DiscountItem.java
+++ src/id/ac/polinema/midterm/DiscountItem.java
@@ -12,18 +12,21 @@ package id.ac.polinema.midterm;
 public class DiscountItem extends Item{
     private float discount;
 
+    //membuat konstruktor berparameter dari class DiscountItem
     public DiscountItem(String name, int price, float amount, float discount) {
         super(name, price, amount);
         this.discount = discount;
     }
     
+    //membuat method untuk menghitung total setelah mendapat diskon
     @Override
     public float getTotalPrice(){
         return (price*amount) - (price*amount*discount);
     }
     
+    //membuat method untuk menampilkan 
     @Override
     public String toString(){
-        return "|"+(name)+" \t\t|"+(price)+"\t\t|"+(amount)+"\t\t|"+(price*amount)+"\t\t|"+getTotalPrice()+"\t"+"\n"+"\t\t|";
+        return "|"+(name)+" \t\t|"+(price)+"\t\t|"+(amount)+"\t\t|"+(price*amount)+"\t\t|"+getTotalPrice()+"\t|"+"\n"+"|"+"-Disc"+"\t\t"+(discount*100)+"%"+"\t\t|"+"\t\t|"+"\t\t|";
     }
 }
