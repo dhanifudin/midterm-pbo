b51a3b2949e42294ff51c8b2e07d21bc65f618a6
WINDOWS 10 <WINDOWS 10@Windows10>
2018-10-18 08:01:23 +0700

    menambahkan komentar pada setiap method

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -14,20 +14,24 @@ public class Item {
     protected String name;
     protected int price;
     protected float amount;
-
+    
+    //membuat konstruktor default untuk class item
     public Item() {
     }
-
+    
+    //membuat konstruktor berparameter dari class Item
     public Item(String name, int price, float amount) {
         this.name = name;
         this.price = price;
         this.amount = amount;
     }
     
+    //membuat method untuk menghitung total
     public float getTotalPrice(){
         return (int)(amount*price);
     }
     
+    //membuat method untuk menampilkan 
     @Override
     public String toString(){
         return "|"+(name)+" \t\t|"+(price)+"\t\t|"+(amount)+"\t\t|"+(price*amount)+"\t\t|";
