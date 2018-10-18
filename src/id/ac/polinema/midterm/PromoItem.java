76bf89477fa2910c975ac40ab3831d5371acbaad
ASUS <ASUS@192.168.56.1>
2018-10-18 08:05:49 +0700

    Menambahkan komentar pada tiap method pada class

--- src/id/ac/polinema/midterm/PromoItem.java
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -10,23 +10,22 @@ package id.ac.polinema.midterm;
  * @author Karinawdyi
  */
 public class PromoItem extends Item{
+    //menambahkan atribut promo
     private float promo;
-
+    
+    //menambahkan konstruktor berparameter 
     public PromoItem(String name, float price, int amount, float promo) {
         super(name, price, amount);
         this.promo = promo;
     }
-    
+    //menambahkan method untuk menghitung Harga Total dengan menggunakan promo
     public float getTotalPrice(){
         return (price*amount) - (amount*promo);
     }
     
+    //menambahkan method untuk @Override
     @Override
     public String toString(){
         return "|" + (name) + "\t\t" + (price) + "\t\t|" +(amount) +"\t\t|" + getTotalPrice() + "\t|" + "\n"+"|"+"-Promo"+"\t\t"+(promo)+"\t\t|"+"\t\t|"+"\t\t|";
     }
-    
-    
-    
-    
 }
--- src/id/ac/polinema/midterm/Transaction.java
+++ src/id/ac/polinema/midterm/Transaction.java
@@ -10,17 +10,19 @@ package id.ac.polinema.midterm;
  * @author dhanifudin
  */
 public class Transaction {
+    //menambahkan atribut pada Transaction yang berasal dari beberapa class sebelumnya
         private Item item;
         private DiscountItem discountItem;
         private PromoItem promoItem;
-
+    
+    //menambahkan method konstruktor berparameter
     public Transaction(Item item, DiscountItem discountItem, PromoItem promoItem) {
         this.item = item;
         this.discountItem = discountItem;
         this.promoItem = promoItem;
     }
         
-        
+        //menambahkan method untuk menampilkan isi dari class ini
 	public void print() {
 		System.out.println("| Nama \t\t| Harga \t| Jumlah \t| Total \t|");
 		System.out.println("|---------------------------------------------------------------|");
