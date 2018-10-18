2dfe47d01832a5618ed98b439e675df9f5f2c378
HP <HP@R1o-mu5>
2018-10-18 08:08:03 +0700

    menambahkan comment

--- src/id/ac/polinema/midterm/PromoItem.java
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -10,17 +10,21 @@ package id.ac.polinema.midterm;
  * @author HP
  */
 public class PromoItem extends Item{
+    //menambahkan atribut
     private float promo;
-
+    
+    //constructor untuk class promoItem
     public PromoItem(String name, float price, int amount, float promo) {
         super(name, price, amount);
         this.promo = promo;
     }
     
+    //method untuk menghitung harga setelah mendapatkan promo
     public float getTotalPrice(){
         return (amount * price) - (promo * amount);
     }
     
+    //untuk menampilka
     @Override
     public String toString(){
         return "|"+name+"\t\t|"+price+"\t|"+amount+"\t\t|"+getTotalPrice()+"\t|\n|-promo\t\t|"+promo+"%\t|\t\t|\t\t|";
