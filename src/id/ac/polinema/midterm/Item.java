742679a80073a660b3c65207ab46164e00cc05a2
HP <HP@R1o-mu5>
2018-10-18 08:06:01 +0700

    menambahkan comment

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -11,23 +11,24 @@ package id.ac.polinema.midterm;
  * @author HP
  */
 public class Item {
+    //membuat atribut 
     protected String name;
     protected float price;
     protected int amount;
-
-    public Item() {
-    }
     
+    //membuat constructor
     public Item(String name, float price, int amount) {
         this.name = name;
         this.price = price;
         this.amount = amount;
     }
     
+    //method untuk menghitung total
     public float getTotalPrice(){
         return (int)price * amount;
     }
     
+    //method untuk menampilkan 
     @Override
     public String toString(){
         return "|"+name+" \t\t"+price+" \t|"+amount+" \t\t|"+(price * amount)+"\t|";
