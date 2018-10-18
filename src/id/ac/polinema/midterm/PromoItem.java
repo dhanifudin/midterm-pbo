d1ea1bcfe405b5bc5a317ba6f2f218a6ace4cfd6
Asus-Pc <Asus-Pc@ARSYAD>
2018-10-18 20:16:12 +0700

    Menambahkan method pada DiscountItem

--- src/id/ac/polinema/midterm/PromoItem.java
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -9,51 +9,17 @@ package id.ac.polinema.midterm;
  *
  * @author Asus-Pc
  */
-public class PromoItem extends Item{
-   
-    private float promo;
-
-    public PromoItem( String name, float price, int amount,float promo) {
-        super(name, price, amount);
-        this.promo = promo;
-    }
+class PromoItem extends Item{
+    protected float promo;
     
-    
- 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public float getPromo() {
-        return promo;
-    }
-
-    public void setPromo(float promo) {
-        this.promo = promo;
-    }
+    public PromoItem(){
         
-        public float PromoItem(){
-            promo = (price * amount) - promo;
-            return promo;
-        }
     }
+
+    public PromoItem(String name, float price, int amount, float promo) {
+        super(name, price, amount);
+        this.promo = (price - promo) * amount;
+    }
+    
+    
+}
