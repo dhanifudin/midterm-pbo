55706f83394f665f5f9b550ab7fb1b94cf102b28
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2018-10-18 08:45:37 +0700

    menambahkan komen deskriptif pada class item

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -11,20 +11,24 @@ package id.ac.polinema.midterm;
  * @author dhanifudin
  */
 public class Item {
+    //menambahkan atribut nama bertipe String
     protected String nama;
+    //menambahkan atribut price bertipe float
     protected float price;
+    //menambahkan atribut amount bertipe int
     protected int amount;
-
+    //menambahkan konstruktor
     public Item(String nama, float price, int amount) {
         this.nama = nama;
         this.price = price;
         this.amount = amount;
     }
-
+    //fungsi getTotalPrice berfungsi untuk menghitung total harga
     public float getTotalPrice() {
+        //mengembalikan nilai dengan mengkalikan price dan amount
         return price*amount;
     }
-
+    //mengubah variable yang berada pada parrent class
     @Override
     public String toString() {
         return nama;
