6ec3ba69ff713d6908bde5675e5dc4d9520cb3ef
KINTUL <KINTUL@DESKTOP-6GMU9JK>
2018-10-18 07:52:35 +0700

    Menambahkan Komentar

--- src/id/ac/polinema/midterm/PromoItem.java
+++ src/id/ac/polinema/midterm/PromoItem.java
@@ -10,17 +10,22 @@ package id.ac.polinema.midterm;
  * @author KINTUL
  */
 class PromoItem extends Item{
+    // menambahkan atribut dengan acess modifier protected agar bisa diakses oleh class yang memilikinya ataupun
+    // class yang merupakan keturunannya
     protected float promo;
     
     public PromoItem(){
         
     }
-
+    // menambahkan konstruktor
     public PromoItem(String name, float price, int amount, float promo) {
         super(name, price, amount);
         this.promo = promo;
     }
-    
+    // menambahkan method setPromoTotal
+    // untuk menghitung promo
+    // yang didapat dari harga dikurang promo yang didapat
+    // baru dikali dengan jumlah barang
     public float setPromoTotal(){
         return (price - promo) * amount;
     }
