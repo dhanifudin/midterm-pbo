583aa6dcd93768b129a2c19dcf671c2eda31b017
Ashfin Ramadhandy E <ashfinramadhandy10@gmail.com>
2018-10-24 07:18:53 +0700

    perbaruan author

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -1,15 +1,6 @@
 package id.ac.polinema.midterm;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author dhanifudin
- */
 public class Item {
  protected String name;
     protected float price;
